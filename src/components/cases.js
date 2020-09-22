624a47b4dba1748a3e8a31df65169dbb16b751af
Diego Ulises <du.ulises@gmail.com>
2020-09-21 20:10:57 -0500

    .

--- src/components/cases.js
+++ src/components/cases.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { ReactComponent as CasesNext } from "../assets/arrow-right.svg";
-//import { ReactComponent as CasesPrev } from "../assets/arrow-left.svg";
 
+import WebDesign from '../assets/DW.png'
 const caseStudies = [
   {
     id: 1,
     subtitle: "Landing pages, e-commerces, diseño responsivo.",
     title: "Diseño Web",
     img: "Diseño Web",
-    link: "/web-design"
+    link: "/web-design",
   },
   {
     id: 2,
     subtitle: "",
     title: "Diseño de Redes Sociales",
     img: "Diseño Redes Sociales",
-    link: "/social-media-design"
+    link: "/social-media-design",
   },
   {
     id: 3,
     subtitle: "",
     title: "Logos, Carteles & Renders",
     img: "Logos, Carteles, Renders",
-    link: "/renders"
+    link: "/renders",
   },
   {
     id: 4,
     subtitle: "",
     title: "Ilustraciones & Bocetos",
     img: "Ilustraciones-Bocetos",
-    link: "/illustrations"
+    link: "/illustrations",
   },
 ];
 
@@ -47,22 +47,18 @@ const Cases = () => {
           </div>
         </div>
         <div className="row">
-          {caseStudies.map((caseItem) => (
-            // <NavLink style="text-decoration: none;" to={caseItem.link} exact>
-              <div className="case" key={caseItem.id}>
-                <div className="case-details">
-                  <span>{caseItem.subtitle}</span>
-                  <h2>{caseItem.title}</h2>
-                </div>
-                <div className="case-image">
-                  <img
-                    src={require(`../assets/${caseItem.img}.png`)}
-                    alt={caseItem.title}
-                  />
-                </div>
-              </div>
-            // </NavLink> 
-          ))}
+          <div className="case">
+            <div className="case-details">
+              <span>Landing pages, e-commerces, diseño responsivo.</span>
+              <h2>Diseño Web</h2>
+            </div>
+            <div className="case-image">
+              <img
+                src={WebDesign}
+                alt="Diseño Web"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
