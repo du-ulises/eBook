266890fe5c6c6839554fe2579475c8d2bd660a6c
Diego Ulises <du.ulises@gmail.com>
2020-09-21 20:22:52 -0500

    Cases

--- src/components/cases.js
+++ src/components/cases.js
@@ -1,37 +1,10 @@
 import React from "react";
 import { ReactComponent as CasesNext } from "../assets/arrow-right.svg";
 
-import WebDesign from '../assets/DW.png'
-const caseStudies = [
-  {
-    id: 1,
-    subtitle: "Landing pages, e-commerces, diseño responsivo.",
-    title: "Diseño Web",
-    img: "Diseño Web",
-    link: "/web-design",
-  },
-  {
-    id: 2,
-    subtitle: "",
-    title: "Diseño de Redes Sociales",
-    img: "Diseño Redes Sociales",
-    link: "/social-media-design",
-  },
-  {
-    id: 3,
-    subtitle: "",
-    title: "Logos, Carteles & Renders",
-    img: "Logos, Carteles, Renders",
-    link: "/renders",
-  },
-  {
-    id: 4,
-    subtitle: "",
-    title: "Ilustraciones & Bocetos",
-    img: "Ilustraciones-Bocetos",
-    link: "/illustrations",
-  },
-];
+import WebDesign from "../assets/DW.png";
+import DRS from "../assets/DRS.png";
+import IB from "../assets/IB.png";
+import Renders from "../assets/LCR.png";
 
 const Cases = () => {
   return (
@@ -53,10 +26,34 @@ const Cases = () => {
               <h2>Diseño Web</h2>
             </div>
             <div className="case-image">
-              <img
-                src={WebDesign}
-                alt="Diseño Web"
-              />
+              <img src={WebDesign} alt="Diseño Web" />
+            </div>
+          </div>
+          <div className="case">
+            <div className="case-details">
+              <span></span>
+              <h2>Diseño de Redes Sociales</h2>
+            </div>
+            <div className="case-image">
+              <img src={DRS} alt="Diseño de Redes Sociales" />
+            </div>
+          </div>
+          <div className="case">
+            <div className="case-details">
+              <span></span>
+              <h2>Logos, Carteles & Renders</h2>
+            </div>
+            <div className="case-image">
+              <img src={Renders} alt="Logos, Carteles & Renders" />
+            </div>
+          </div>
+          <div className="case">
+            <div className="case-details">
+              <span></span>
+              <h2>Ilustraciones & Bocetos</h2>
+            </div>
+            <div className="case-image">
+              <img src={IB} alt="Ilustraciones & Bocetos" />
             </div>
           </div>
         </div>
